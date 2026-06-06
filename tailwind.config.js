0d325fd49f7ff86f2489b6f55a405a5408200e77
vigorePro <vigore0102@gmail.com>
2026-06-06 09:10:05 -0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+        './lib/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+      ],
+      theme: { extend: {} },
+  plugins: [],
+    }
