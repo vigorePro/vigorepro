6730a0eaf863e117b562d580e96be70f758758ec
vigorePro <vigore0102@gmail.com>
2026-06-10 12:09:16 -0300

    feat: tailwind config com fontes e cores Foodu

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-        './app/**/*.{js,ts,jsx,tsx,mdx}',
-        './lib/**/*.{js,ts,jsx,tsx}',
-        './components/**/*.{js,ts,jsx,tsx,mdx}',
-      ],
-      theme: { extend: {} },
-  plugins: [],
-    }
+    content: [
+          './app/**/*.{js,ts,jsx,tsx,mdx}',
+          './lib/**/*.{js,ts,jsx,tsx}',
+          './components/**/*.{js,ts,jsx,tsx,mdx}',
+        ],
+    theme: {
+          extend: {
+                  fontFamily: {
+                            jost: ['var(--font-jost)', 'sans-serif'],
+                            oswald: ['var(--font-oswald)', 'sans-serif'],
+                            dancing: ['var(--font-dancing)', 'cursive'],
+                  },
+                  colors: {
+                            foodu: {
+                                        red: '#eb0029',
+                                        orange: '#f76e2a',
+                                        cream: '#faf1df',
+                                        dark: '#04000b',
+                            },
+                  },
+          },
+    },
+    plugins: [],
+}
